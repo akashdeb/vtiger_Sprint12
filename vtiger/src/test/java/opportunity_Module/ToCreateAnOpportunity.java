c3aa20b209c7da28ffd6ab35f0b6fe54f60c91d9
akashdeb <akashdeb87@gmail.com>
2023-10-18 14:22:10 +0530

    Added Opportunity module

---
+++ vtiger/src/test/java/opportunity_Module/ToCreateAnOpportunity.java
@@ -0,0 +1,14 @@
+package opportunity_Module;
+
+import org.testng.annotations.Test;
+
+import genericUtility.BaseClass;
+
+public class ToCreateAnOpportunity extends BaseClass{
+	
+	@Test
+	public void createOpportunity() {
+		System.out.println("Opportunity is created");
+	}
+
+}
